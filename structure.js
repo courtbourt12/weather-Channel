a973b0adc02cf3c39d7ca2db754cd21a5d0d2497
courtney long <courtneylong877@gmail.com>
2021-09-27 17:47:58 -0400

    Basic HTML done.

---
+++ structure.js
@@ -0,0 +1,12 @@
+var APIKey = "62af3ed8671f158493e172339537199e";
+
+// Variables for API calls.
+
+var city;
+var temperature;
+var date;
+var state;
+var country;
+var humidity;
+var UVIndex;
+var windSpeed;
