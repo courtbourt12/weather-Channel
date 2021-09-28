a6a0bef5f9b8c0a8f1f44825c6326839c2bf0cbe
courtney long <courtneylong877@gmail.com>
2021-09-28 17:39:17 -0400

    Fetch complete, all info needed is entered into HTML

--- structure.js
+++ structure.js
@@ -26,8 +26,56 @@ function cityLookup() {
 
     fetch(geocoding)
     .then(function(response) {
+      return (response.json())
+    }).then(function(data) {
+        console.log(data);
+        var latitude = data[0].lat;
+        var longitude = data[0].lon;
+    
+        console.log(latitude);
+        console.log(longitude);
+
+        weatherForcast();
+
+        function weatherForcast() {
         
+           var weatherInfo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + APIKey;
+        
+               fetch(weatherInfo)
+               .then(function(response) {
+                   return (response.json())
+               }).then(function(data) {
+                   console.log(data);
+                
+                document.querySelector(".temperature").innerHTML = data.current.temp + "°F";
+                document.querySelector(".wind").innerHTML = data.current.wind_speed + "mph";
+                document.querySelector(".humidity").innerHTML = data.current.humidity + "%";
+                document.querySelector(".uv").innerHTML = data.current.uvi;
+                
+                document.querySelector("#temp2").innerHTML = data.daily[0].temp.day + "°F";
+                document.querySelector("#wind2").innerHTML = data.daily[0].wind_speed + "mph";
+                document.querySelector("#humidity2").innerHTML = data.daily[0].humidity + "%";
+
+                document.querySelector("#temp3").innerHTML = data.daily[1].temp.day + "°F";
+                document.querySelector("#wind3").innerHTML = data.daily[1].wind_speed + "mph";
+                document.querySelector("#humidity3").innerHTML = data.daily[1].humidity + "%";
+
+                document.querySelector("#temp4").innerHTML = data.daily[2].temp.day + "°F";
+                document.querySelector("#wind4").innerHTML = data.daily[2].wind_speed + "mph";
+                document.querySelector("#humidity4").innerHTML = data.daily[2].humidity + "%";
+
+                document.querySelector("#temp5").innerHTML = data.daily[3].temp.day + "°F";
+                document.querySelector("#wind5").innerHTML = data.daily[3].wind_speed + "mph";
+                document.querySelector("#humidity5").innerHTML = data.daily[3].humidity + "%";
+
+                   
+               })
+        
+        }
     })
+
 };
 
  clickHere.addEventListener("click", cityLookup);
+
+ // 
