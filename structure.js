ea52f1158617e045bd33130c120a4d63c9579557
courtney long <courtneylong877@gmail.com>
2021-09-28 16:56:07 -0400

    Geolocation for cities added

--- structure.js
+++ structure.js
@@ -10,3 +10,24 @@ var country;
 var humidity;
 var UVIndex;
 var windSpeed;
+
+// Setting up the city search.
+
+var citySearch = document.querySelector("#searchBar");
+
+var clickHere = document.getElementById("searchNow");
+
+function cityLookup() {
+  
+    var citySelected = document.querySelector("#searchBar").value;
+    var geocoding = "http://api.openweathermap.org/geo/1.0/direct?q=" + citySelected + "&appid=" + APIKey;
+
+    console.log(geocoding);
+
+    fetch(geocoding)
+    .then(function(response) {
+        
+    })
+};
+
+ clickHere.addEventListener("click", cityLookup);
